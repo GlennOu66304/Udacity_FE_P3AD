6de2fe899fe00ddd99d80a7070841c05929654bb
Glenn <glenou@yahoo.com>
2022-09-17 02:52:29 +0800

    update data

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 # Demo Link:
 Udacity_FE_P3AD: [Udacity_FE_P3AD](https://glennou66304.github.io/Udacity_FE_P3AD/).  
 
+
+side bar section  
+Offcanvas components   
+https://getbootstrap.com/docs/5.2/components/offcanvas/      
+
+
 # Udacity_FE_P3AD
 我要如何完成此项目？
 
--- js/app.js
+++ js/app.js
@@ -4,55 +4,347 @@ var map, inforWindow;
 
 // source of data json format
 
-var placesData = [
+var placesJson = [
   {
+    title:"Alaska",
+    position:{
+    lat:61.3850,
+    lng:-152.2683
+    }
+  },
+  {
+    title:"Alabama",
+    position:{
+    lat:32.7990,
+    lng:-86.8073
+    }
+  },
+  {
+    title:"Arkansas",
+    position:{
+    lat:34.9513,
+
+    lng:-92.3809
+    }
+  },
+  {
+    title:"Arizona",
+    position:{
+    lat:33.7712,
+    lng:-111.3877
+    }
+  },
+  {
+    title:"California",
+    position:{
+    lat:36.1700,
+    lng:-119.7462
+    }
+  },
+  {
+    title:"Colorado",
+    position:{
+    lat:39.0646,
+    lng:-105.3272
+    }
+  },
+  {
+    title:"Connecticut",
+    position:{
+    lat:41.5834,
+    lng:-72.7622
+    }
+  },
+  {
+    title:"Delaware",
+    position:{
+    lat:39.3498,
+    lng:-75.5148
+    }
+  },
+  {
+    title:"Florida",
+    position:{
+    lat:27.8333,
+    lng:-81.7170
+  }
+  },
+  {
+    title:"Georgia",
+    position:{
+    lat:32.9866,
+    lng:-83.6487 }
+  },
+  {
+    title:"Hawaii",
+    position:{
+    lat:21.1098,
+    lng:-157.5311 }
+  },
+  {
+    title:"Iowa",
+    position:{
+    lat:42.0046,
+    lng:-93.2140 }
+  },
+  {
+    title:"Idaho",
+    position:{
+    lat:44.2394,
+    lng:-114.5103 }
+  },
+  {
+    title:"Illinois",
+    position:{
+    lat:40.3363,
+    lng:-89.0022 }
+  },
+  {
+    title:"Indiana",
+    position:{
+    lat:39.8647,
+    lng:-86.2604 }
+  },
+  {
+    title:"Kansas",
+    position:{
+    lat:38.5111,
+    lng:-96.8005 }
+  },
+  {
+    title:"Kentucky",
+    position:{
+    lat:37.6690,
+    lng:-84.6514 }
+  },
+  {
+    title:"Louisiana",
+    position:{
+    lat:31.1801,
+    lng:-91.8749 }
+  },
+  {
+    title:"Massachusetts",
+    position:{
+    lat:42.2373,
+    lng:-71.5314 }
+  },
+  {
+    title:"Maryland",
+    position:{
+    lat:39.0724,
+    lng:-76.7902 }
+  },
+  {
+    title:"Maine",
+    position:{
+    lat:44.6074,
+    lng:-69.3977 }
+  },
+  {
+    title:"Michigan",
+    position:{
+    lat:43.3504,
+    lng:-84.5603 }
+  },
+  {
+    title:"Minnesota",
+    position:{
+    lat:45.7326,
+
+    lng:-93.9196 }
+  },
+  {
+    title:"Missouri",
+    position:{
+    lat:38.4623,
+    lng:-92.3020 }
+  },
+  {
+    title:"Mississipositionpositioni",
+    position:{
+    lat:32.7673,
+    lng:-89.6812 }
+  },
+  {
+    title:"Montana",
+    position:{
+    lat:46.9048,
+    lng:-110.3261 }
+  },
+  {
+    title:"North Carolina",
+    position:{
+    lat:35.6411,
+    lng:-79.8431 }
+  },
+  {
+    title:"North Dakota",
+    position:{
+    lat:47.5362,
+    lng:-99.7930 }
+  },
+  {
+    title:"Nebraska",
+    position:{
+    lat:41.1289,
+    lng:-98.2883 }
+  },
+  {
+    title:"New Hampositionshire",
+    position:{
+    lat:43.4108,
+    lng:-71.5653 }
+  },
+  {
+    title:"New Jersey",
+    position:{
+    lat:40.3140,
+    lng:-74.5089 }
+  },
+  {
+    title:"New Mexico",
+    position:{
+    lat:34.8375,
+    lng:-106.2371 }
+  },
+  {
+    title:"Nevada",
+    position:{
+    lat:38.4199,
+    lng:-117.1219 }
+  },
+  {
+    title:"New York",
+    position:{
+    lat:42.1497,
+    lng:-74.9384 }
+  },
+  {
+    title:"Ohio",
+    position:{
+    lat:40.3736,
+    lng:-82.7755 }
+    }
+  ,
+  {
+    title:"Oklahoma",
+    position:{
+    lat:35.5376,
+    lng:-96.9247 }
+    }
+,
+  {
+    title:"Oregon",
+    position:{
+    lat:44.5672,
+    lng:-122.1269 }
+    }
+,
+  {
+    title:"Pennsylvania",
+    position:{
+    lat:40.5773,
+    lng:-77.2640 }
+    }
+,
+  {
+    title:"Rhode Island",
+    position:{
+    lat:41.6772,
+    lng:-71.5101 }
+    }
+,
+  {
+    title:"South Carolina",
+    position:{
+    lat:33.8191,
+    lng:-80.9066 }
+    }
+ ,
+  {
+    title:"South Dakota",
+    position:{
+    lat:44.2853,
+    lng:-99.4632 }
+    }
+,
+  {
+    title:"Tennessee",
+    position:{
+    lat:35.7449,
+    lng:-86.7489 }
+    }
+ ,
+  {
+    title:"Texas",
+    position:{
+    lat:31.1060,
+    lng:-97.6475 }
+    }
+  ,
+  {
+    title:"Utah",
+    position:{
+    lat:40.1135,
+    lng:-111.8535 }
+    }
+,
+  {
+    title:"Virginia",
+    position:{
+    lat:37.7680,
+    lng:-78.2057 }
+    }
+,
+  {
+    title:"Vermont",
+    position:{
+    lat:44.0407,
+    lng:-72.7093 }
+    }
+ ,
+  {
+    title:"Washington",
+    position:{
+    lat:47.3917,
+    lng:-121.5708 }
+    }
+ ,
+  {
+    title:"Wisconsin",
+    position:{
+    lat:44.2563,
+    lng:-89.6385 }
+    }
+,
+
+  {
+    title:"West Virginia",
+    position:{
+    lat:38.4680,
+    lng:-80.9696 }
+    }
+ ,
+  {
+    title:"Wyoming",
     positon: {
-      lat: 25.025338,
-      lng: 121.579771,
+    lat:42.7475,
+    lng:-107.2085
     },
-    title: "象山自然步道",
-  },
+    }
+  ]
+// marker data source
 
-  {
-    position: {
-      lat: 24.220837,
-      lng: 121.6931,
-    },
-    title: "花蓮清水斷崖",
-  },
+// console.log(placesJson);
 
-  {
-    position: {
-      lat: 24.721554,
-      lng: 120.901959,
-    },
-    title: "佛光山",
-  },
-
-  {
-    position: {
-      lat: 22.610289,
-      lng: 120.301898,
-    },
-    title: "高雄市立圖書館總館",
-  },
-
-  {
-    position: {
-      lat: 25.020343,
-      lng: 120.301898,
-    },
-    title: "國立臺灣大學",
-  },
-];
-
-// mocek up data function, no use in this project
-var Place = function (data) {
+// do not change this function to arrow function, will cause the constructor error
+var Markers = function (data) {
   var self = this;
   this.title = data.title;
   this.position = data.position;
 
-  this.visible = ko.computed(function () {
+  this.visible = ko.computed(() => {
     var re = filterText().toLowerCase();
     var placeName = self.title.toLowerCase();
     return placeName.indexOf(re) != -1;
@@ -64,7 +356,7 @@ var Place = function (data) {
     animation: google.maps.Animation.DROP,
   });
 
-  google.maps.event.addListener(self.marker, "click", function () {
+  google.maps.event.addListener(self.marker, "click", () => {
     infoWindow.setContent(self.title);
     infoWindow.open(map, self.marker);
 
@@ -95,9 +387,10 @@ var Place = function (data) {
   });
 };
 
+// knouck out js function use
 var viewModel = function () {
   this.isShowSidebar = ko.observable(true);
-//   close or open the side bar
+  //   close or open the side bar
   this.toggleSidebar = function () {
     var x = document.getElementById("drawer");
     if (x.style.display === "none") {
@@ -107,34 +400,41 @@ var viewModel = function () {
     }
   };
 
-//   inilized dropdonw data list
+  //   inilized dropdonw data list
   var self = this;
   this.placesList = [];
 
-  placesData.forEach(function (place) {
-    self.placesList.push(new Place(place));
+  // push the makers to the map, use the markers function here
+  placesJson.forEach((place) => {
+    // push the makers to the map, use the markers function here
+    self.placesList.push(new Markers(place));
+    // console.log(this.placesList)
   });
 
-// set marker function
-  this.placesList.forEach(function (place) {
+
+  // set marker function
+  this.placesList.forEach((place) => {
     place.marker.setMap(map, place.position);
   });
 
-// search result list display
-  this.filteredList = ko.computed(function () {
+  // search result list display
+  this.filteredList = ko.computed(() => {
     var result = [];
-    self.placesList.forEach(function (place) {
+    self.placesList.forEach((place) => {
       if (place.visible()) {
         result.push(place);
+        // console.log(result)
         place.marker.setMap(map, place.position);
+       
       } else {
         place.marker.setMap(null);
       }
     });
+
     return result;
   });
 
-//  show the marker data
+  //  show or close the marker display
   this.listClick = function (place) {
     google.maps.event.trigger(place.marker, "click");
   };
@@ -142,19 +442,19 @@ var viewModel = function () {
 
 // initialize the map function
 function start() {
-// render the map
+  // render the map
   map = new google.maps.Map(document.getElementById("map"), {
-    center: placesData[2].position,
-    zoom: 8,
+    center: placesJson[2].position,
+    zoom: 5,
   });
-
 
   infoWindow = new google.maps.InfoWindow();
 
-//   knockout function binding here
+  //   knockout function binding here
   ko.applyBindings(new viewModel());
 }
 
 function googleError() {
   alert("google map wrong!");
 }
+
